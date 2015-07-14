9923627e58fd4eea58e8c6cb1623765b7ee9be58
Kim Kulling <kim.kulling@googlemail.com>
2015-07-14 18:52:39 +0200

    Debugging: add validate tool.

--- include/osre/Debugging/osre_debugging.h
+++ include/osre/Debugging/osre_debugging.h
@@ -1,10 +1,38 @@
+/*-----------------------------------------------------------------------------------------------
+The MIT License (MIT)
+
+Copyright (c) 2015 OSRE ( Open Source Render Engine ) by Kim Kulling
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-----------------------------------------------------------------------------------------------*/
 #pragma once
 
 #include <cassert>
+#include <osre/Common/Logger.h>
+#include <osre/Common/osre_common.h>
+
+static void handleFatal( const OSRE::String &file, int line, OSRE::String &msg ) {
+    OSRE::Common::fatalPrint( "Assertion", file, line, msg );
+}
 
 #ifdef _DEBUG
-#  define osre_validate( statement, msg ) assert( statement )
+#  define osre_validate( statement, msg ) if ( !(statement) ) handleFatal( __FILE__, __LINE__,  msg );
 #else
-#  define osre_validate( (statement), (msg) ) 
+#  define osre_validate( statement, msg ) 
 #endif
 
