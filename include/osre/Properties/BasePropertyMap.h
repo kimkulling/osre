85759c9c4bf2340f70a1850cd90928baae6ee7f4
Kim Kulling <kim.kulling@googlemail.com>
2015-12-07 18:52:34 +0100

    Rename IPropertyMap.h to AbstractPropertyMap.h

--- include/osre/Properties/BasePropertyMap.h
+++ include/osre/Properties/BasePropertyMap.h
@@ -22,7 +22,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 -----------------------------------------------------------------------------------------------*/
 #pragma once
 
-#include <osre/Properties/IPropertyMap.h>
+#include <osre/Properties/AbstractPropertyMap.h>
 
 #include <map>
 
