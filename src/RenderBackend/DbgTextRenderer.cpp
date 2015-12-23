016823552371c7528f47605696a92cc9b5232814
Kim Kulling <kim.kulling@googlemail.com>
2015-12-23 20:16:52 +0100

    DbgTextRenderer: remove usage of createVertexData.

--- src/RenderBackend/DbgTextRenderer.cpp
+++ src/RenderBackend/DbgTextRenderer.cpp
@@ -1,3 +1,25 @@
+/*-----------------------------------------------------------------------------------------------
+The MIT License (MIT)
+
+Copyright (c) 2015 OSRE ( Open Source Render Engine ) by Kim Kulling
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-----------------------------------------------------------------------------------------------*/
 #include <osre/RenderBackend/DbgTextRenderer.h>
 #include <osre/RenderBackend/RenderCommon.h>
 
@@ -5,33 +27,6 @@
 
 namespace OSRE {
 namespace RenderBackend {
-
-extern const unsigned char *glyph[];
-
-static const ui32 NumChars = 128;
-
-static const String VsSrc =
-	"layout(location = 0) in vec3 position;	      // object space vertex position\n"
-	"layout(location = 1) in vec3 normal;	            // object space vertex normal\n"
-	"layout(location = 2) in vec2 texcoord0;	        // texture coordinate\n"
-	"out vec2 vUV;\n"
-	"\n"
-	"void main() {\n"
-	"    vec3 vertexPosition_homoneneousspace = position;\n"
-	"    gl_Position = vec4(position, 1 );\n"
-	"    // UV of the vertex. No special space for this one.\n"
-	"    UV = texcoord0;\n"
-	"};\n";
-
-static const String FsSrc =
-	"in vec2 vUV;\n"
-	"// Output data\n"
-	"out vec4 vFragColor;\n"
-	"uniform sampler2D tex0;\n"
-
-	"void main() {\n"
-	"    vFragColor = texture( tex0, vUV );\n"
-	"};\n";
 
 TextRenderer2D::TextRenderer2D()
 : m_data( nullptr )
@@ -46,63 +41,6 @@ TextRenderer2D::TextRenderer2D()
 
 TextRenderer2D::~TextRenderer2D() {
 	clear();
-}
-
-void TextRenderer2D::setupVertexData() {
-	std::vector<RenderVert> vertices;
-	for (ui32 i = 0; i < m_text.size(); i++) {
-		RenderVert vert[ 4 ];
-
-		vert[ 0 ].position = glm::vec3(m_xCursor + i*m_sx, m_yCursor + m_sy, 0);
-		vert[ 1 ].position = glm::vec3(m_xCursor + i*m_sx + m_sx, m_yCursor + m_sy, 0);
-		vert[ 2 ].position = glm::vec3(m_xCursor + i*m_sx + m_sx, m_yCursor, 0);
-		vert[ 3 ].position = glm::vec3(m_xCursor + i*m_sx, m_yCursor, 0);
-
-		char character = m_text[ i ];
-		float uv_x = ( character % 16 ) / 16.0f;
-		float uv_y = ( character / 16 ) / 16.0f;
-		vert[ 0 ].tex0 = glm::vec2(uv_x, 1.0f - uv_y);
-		vert[ 1 ].tex0 = glm::vec2(uv_x + 1.0f / 16.0f, 1.0f - uv_y);
-		vert[ 2 ].tex0 = glm::vec2(uv_x + 1.0f / 16.0f, 1.0f - ( uv_y + 1.0f / 16.0f ));
-		vert[ 3 ].tex0 = glm::vec2(uv_x, 1.0f - ( uv_y + 1.0f / 16.0f ));
-
-		for (ui32 j = 0; j < 4; j++) {
-			vertices.push_back(vert[ j ]);
-		}
-	}
-	
-	const ui32 vertSize(vertices.size() * sizeof(RenderVert));
-	if ( m_data->m_size < vertSize) {
-		if (nullptr != m_data) {
-			BufferData::free( m_data );
-		}
-		m_data = BufferData::alloc( VertexBuffer, vertSize, ReadWrite );
-		::memcpy( m_data->m_pData, &vertices[ 0 ], vertSize );
-	}
-
-    StaticGeometry *geo = StaticGeometry::create();
-	geo->m_numPrimGroups = m_text.size()*2;
-	geo->m_pPrimGroups = new PrimitiveGroup[ geo->m_numPrimGroups ];
-	geo->m_pPrimGroups[ 0 ].m_indexType = UnsignedShort;
-	geo->m_pPrimGroups[ 0 ].m_numPrimitives = 3 * geo->m_numPrimGroups;
-	geo->m_pPrimGroups[ 0 ].m_primitive = TriangleList;
-	geo->m_pPrimGroups[ 0 ].m_startIndex = 0;
-
-	// setup material
-	geo->m_material = new Material;
-	geo->m_material->m_numTextures = 0;
-	geo->m_material->m_type = ShaderMaterial;
-	geo->m_material->m_pShader = new Shader;
-	geo->m_material->m_pShader->m_src[ SH_VertexShaderType ]   = VsSrc;
-	geo->m_material->m_pShader->m_src[ SH_FragmentShaderType ] = FsSrc;
-
-	// setup shader attributes and variables
-	if (nullptr != geo->m_material->m_pShader) {
-		ui32 numAttribs(ColorVert::getNumAttributes());
-		const String *attribs(ColorVert::getAttributes());
-		geo->m_material->m_pShader->m_attributes.add(attribs, numAttribs);
-		geo->m_material->m_pShader->m_parameters.add("MVP");
-	}
 }
 
 void TextRenderer2D::setCursor( ui32  x, ui32 y ) {
