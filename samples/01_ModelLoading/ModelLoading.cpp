ede749efcaba4070e6520ef4c7eb2b2eeb733e2f
Kim Kulling <kim.kulling@googlemail.com>
2016-05-19 19:10:35 +0200

    Prepare modelloading sample.

---
+++ samples/01_ModelLoading/ModelLoading.cpp
@@ -0,0 +1,103 @@
+/*-----------------------------------------------------------------------------------------------
+The MIT License (MIT)
+
+Copyright (c) 2015 OSRE ( Open Source Render Engine ) by Kim Kulling
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-----------------------------------------------------------------------------------------------*/
+#include <osre/App/AppBase.h>
+#include <osre/Properties/Settings.h>
+#include <osre/Common/Logger.h>
+#include <osre/Scene/GeometryBuilder.h>
+#include <osre/Scene/Stage.h>
+#include <osre/Scene/Node.h>
+#include <osre/Assets/AssetRegistry.h>
+#include <osre/RenderBackend/RenderCommon.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace OSRE;
+using namespace OSRE::RenderBackend;
+
+// To identify local log entries 
+static const String Tag    = "HelloWorld"; 
+
+// The example application, will create the render environment and render a simple triangle onto it
+class HelloWorldApp : public App::AppBase {
+    Scene::Stage *m_stage;
+    TransformMatrixBlock m_transformMatrix;
+
+public:
+    HelloWorldApp( int argc, char *argv[] )
+    : AppBase( argc, argv )
+    , m_stage( nullptr ) {
+        // empty
+    }
+
+    virtual ~HelloWorldApp() {
+        // empty
+    }
+
+protected:
+    void createStaticText() {
+
+    }
+
+    virtual bool onCreate( Properties::Settings *settings = nullptr ) {
+        Properties::Settings *baseSettings( AppBase::getSettings() );
+        if ( nullptr == baseSettings ) {
+            return false;
+        }
+       
+        baseSettings->setString( Properties::Settings::WindowsTitle, "HelloWorld!" );
+        if ( !AppBase::onCreate( baseSettings ) ) {
+            return false;
+        }
+
+#ifdef OSRE_WINDOWS
+        Assets::AssetRegistry::getInstance()->registerAssetPath( "assets", "../../media" );
+#else
+        Assets::AssetRegistry::getInstance()->registerAssetPath( "assets", "../media" );
+#endif 
+
+        m_stage = AppBase::createStage( "HelloWorld" );
+        AppBase::activateStage( m_stage->getName() );
+
+        Scene::Node *geoNode = m_stage->addNode( "geo", nullptr );
+        Scene::GeometryBuilder myBuilder;
+        RenderBackend::StaticGeometry *geo = myBuilder.allocTriangles( ColorVertex );
+        if( nullptr != geo ) {
+			m_transformMatrix.m_model = glm::rotate(m_transformMatrix.m_model, 0.0f, glm::vec3(1, 1, 0));
+
+			Parameter *parameter = Parameter::create( "MVP", PT_Mat4 );
+			::memcpy(parameter->m_data.m_data, glm::value_ptr(m_transformMatrix.m_projection*m_transformMatrix.m_view*m_transformMatrix.m_model), sizeof(glm::mat4));
+
+			geo->m_material->m_parameters = parameter;
+            geo->m_material->m_numParameters++;
+			//geo->m_numParameter++;
+
+			geoNode->addGeometry( geo );
+		}
+
+        return true;
+    }
+};
+
+OSRE_MAIN( HelloWorldApp )
---
+++ samples/01_ModelLoading/README.md
@@ -0,0 +1,95 @@
+# Hello World
+![HelloWorld](../../media/Images/HelloWorld.png)
+This sample just shows a simple HelloWorld application. The creation of the render window and the base setup is included.
+
+## The main application
+At first we are including all needed dependecies, use the OSRE namespace and the Renderbackend-namespace. The we will generate our simple model-, view- and projection-matrix.
+```cpp
+#include <osre/App/AppBase.h>
+#include <osre/Properties/Settings.h>
+#include <osre/Common/Logger.h>
+#include <osre/Scene/GeometryBuilder.h>
+#include <osre/Scene/Stage.h>
+#include <osre/Scene/Node.h>
+#include <osre/RenderBackend/RenderCommon.h>
+
+// for the transform ops
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace OSRE;
+using namespace OSRE::RenderBackend;
+
+// to identify local log entries 
+static const String Tag    = "HelloWorld"; 
+```
+
+To start OSRE you cann use the base class for main applications. Just rderive from App::AppBase:
+
+```cpp
+class HelloWorldApp : public App::AppBase {
+    Scene::Stage *m_stage;
+    TransformMatrixBlock m_transformMatrix;
+
+public:
+    HelloWorldApp( int argc, char *argv[] )
+    : AppBase( argc, argv )
+    , m_stage( nullptr ) {
+
+    }
+
+    virtual ~HelloWorldApp() {
+
+    }
+
+protected:
+    virtual bool onCreate( Properties::Settings *config = nullptr ) {
+        AppBase::getConfig()->setString( Properties::Settings::WindowsTitle, "HelloWorld!" );
+
+        if( !AppBase::onCreate( config ) ) {
+            return false;
+        }
+
+        m_stage = AppBase::createStage( "HelloWorld" );
+        Scene::Node *geoNode = m_stage->addNode( "geo", nullptr );
+        Scene::GeometryBuilder myBuilder;
+        RenderBackend::Geometry *geo = myBuilder.createTriangle();
+        if( nullptr != geo ) {
+                m_transformMatrix.m_model = glm::rotate(m_transformMatrix.m_model, 0.0f, glm::vec3(1, 1, 0));
+
+                Parameter *parameter = Parameter::create("MVP", PT_Mat4);
+                ::memcpy(parameter->m_data.m_data,glm::value_ptr(m_transformMatrix.m_projection*m_transformMatrix.m_view*m_transformMatrix.m_model), sizeof(glm::mat4));
+
+                geo->m_parameter = parameter;
+                geo->m_numParameter++;
+
+                geoNode->addGeometry(geo);
+        }
+
+        return true;
+    }
+
+    virtual void onUpdate()  {
+        m_stage->update();
+    }
+};
+```
+At first we add a stage as an member attribute of your Hello-World. A stage stores the current scene.
+Then we need a transformation to move somthing inside of your scene, so we need another member attribute for the transformation.
+
+Now we create the constructor and the destructor. 
+
+We create the stag in the onCreate callback. When calling AbbBase::create this callback will be called by OSRE. We are creating a simple triangle, 
+storing this in a geometry node, add this to the stage. Now we adding the attributes of our vertex layout, creating a parameter to set the transformation 
+to a parameter of the geometry.
+
+The onUpdate-callback will be called once per frame. We just updating the stage and thats all.
+
+## The main program
+In your main you just need to create the application instance and call create.
+Afterwards you enter the main loop, where you can react on events, update your app and request the rendering.
+```cpp
+OSRE_MAIN( HelloWorldApp )
+```
+
