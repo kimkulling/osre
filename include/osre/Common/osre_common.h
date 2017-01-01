6ca0bddad35e553fe8bc8735525923b93ad29b58
Kim Kulling <kim.kulling@googlemail.com>
2017-01-01 17:37:07 +0100

    Common: add missing header.

--- include/osre/Common/osre_common.h
+++ include/osre/Common/osre_common.h
@@ -26,6 +26,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <string.h>
 #include <stdio.h>
 #include <cstddef>
+#include <cmath>
 
 #ifndef _WIN32
 #  include <inttypes.h>
