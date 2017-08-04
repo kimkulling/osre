4b167fd8643002dc4d4c34162a1f8748c39a4fef
Kim Kulling <kim.kulling@googlemail.com>
2017-08-05 00:14:20 +0200

    scene: remove unused include-

--- include/osre/Scene/Node.h
+++ include/osre/Scene/Node.h
@@ -27,7 +27,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <cppcore/Container/TArray.h>
 #include <cppcore/Container/THashMap.h>
-#include "3dparty/assimp/code/ColladaLoader.h"
+//#include "3dparty/assimp/code/ColladaLoader.h"
 
 namespace OSRE {
 
