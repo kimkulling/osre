4e5540a84163ed3d1a22dd416f9ed6d2bc889ad2
Kim Kulling <kim.kulling@googlemail.com>
2018-02-01 20:34:46 +0100

    dev: update license info and add resource cache template.

--- include/osre/RenderBackend/RenderStates.h
+++ include/osre/RenderBackend/RenderStates.h
@@ -22,39 +22,47 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 -----------------------------------------------------------------------------------------------*/
 #pragma once
 
-#include <osre/Common/osre_common.h>
+#include <osre/RenderBackend/RenderCommon.h>
+#include <osre/RenderBackend/ClearState.h>
+#include <osre/RenderBackend/BlendState.h>
+#include <osre/RenderBackend/CullState.h>
+#include <osre/RenderBackend/SamplerState.h>
+#include <osre/RenderBackend/StencilState.h>
 
 namespace OSRE {
-namespace Platform  {
+namespace RenderBackend {
 
-//-------------------------------------------------------------------------------------------------
-///	@ingroup	Engine
-///
-///	@brief  The abstract interface for a thread event.
-//-------------------------------------------------------------------------------------------------
-class OSRE_EXPORT AbstractThreadEvent {
-public:
-	///	@brief	The class destructor, virtual.
-	virtual ~AbstractThreadEvent();
+struct RenderStates {
+    TransformState m_transformState;
+    PolygonState   m_polygonState;
+    BlendState     m_blendState;
+    CullState      m_cullState;
+    SamplerState   m_samplerState;
+    StencilState   m_stensilState;
+    bool           m_applied;
 
-	///	@brief	Signals the class.
-	virtual void signal() = 0;
+    RenderStates()
+    : m_transformState()
+    , m_polygonState()
+    , m_blendState()
+    , m_cullState()
+    , m_samplerState()
+    , m_stensilState()
+    , m_applied( false ) {
+        // empty
+    }
 
-	///	@brief	Waits until one condition is signaled.
-	virtual void waitForOne() = 0;
-
-    ///	@brief	Waits until all conditions are signaled.
-    virtual void waitForAll() = 0;
-
-	///	@brief	Wait for a signal or the timeout has passed.
-	///	@param	ms	[in] The timeout in ms.
-	virtual void waitForTimeout( ui32 ms ) = 0;
+    bool isEqual( const TransformState &transformState, const PolygonState &polygonState, const CullState &cullstate, const BlendState &blendState,
+        const SamplerState &samplerState, const StencilState &stencilState ) const {
+        return ( transformState == m_transformState
+            && polygonState == m_polygonState
+            && blendState == m_blendState
+            && cullstate == m_cullState
+            && samplerState == m_samplerState
+            && stencilState == m_stensilState );
+    }
 };
 
-inline
-AbstractThreadEvent::~AbstractThreadEvent( ) {
-	// empty
-}
-
-} // Namespace Platform
+} // Namespace RenderBackend
 } // Namespace OSRE
+
