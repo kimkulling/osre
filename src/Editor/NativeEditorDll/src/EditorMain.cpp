f073cc2cbfa747f9559f703a94422837dbbe4edc
Kim Kulling <kim.kulling@googlemail.com>
2018-04-17 19:04:32 +0200

    Editor: prepare nativedll wrapper for c#-based editor.

---
+++ src/Editor/NativeEditorDll/src/EditorMain.cpp
@@ -0,0 +1,19 @@
+#include <osre/Common/osre_common.h>
+
+using namespace ::OSRE;
+
+int CreateEditorApp() {
+    return 0;
+}
+
+int EditorUpdate() {
+    return 0;
+}
+
+int EditorRequestNextFrame() {
+    return 0;
+}
+
+int DestroyEditorApp() {
+    return 0;
+}
---
+++ src/Editor/NativeEditorDll/src/EditorMain.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <osre/Common/osre_common.h>
+
+#ifdef OSRE_WINDOWS
+#  define TAG_DLL_EXPORT __declspec(dllexport)
+#  define TAG_DLL_IMPORT __declspec(dllimport )
+#endif 
+
+#define OSRE_EDITOR_BUILD_EXPORT
+
+#ifdef OSRE_WINDOWS
+#   ifdef OSRE_EDITOR_BUILD_EXPORT
+#       define OSRE_EDITOR_EXPORT TAG_DLL_EXPORT
+#   else
+#       define OSRE_EDITOR_EXPORT TAG_DLL_IMPORT
+#   endif
+#else
+#   define OSRE_EDITOR_EXPORT  __attribute__ ((visibility("default")))
+#endif
+
+extern "C" OSRE_EDITOR_EXPORT int CreateEditorApp();
+
+extern "C" OSRE_EDITOR_EXPORT int EditorUpdate();
+
+extern "C" OSRE_EDITOR_EXPORT int EditorRequestNextFrame();
+
+extern "C" OSRE_EDITOR_EXPORT int DestroyEditorApp();
