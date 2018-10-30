48be055cd0fdab00307076b0adae2279948cc302
kimkulling <kim.kulling@googlemail.com>
2018-10-30 17:45:44 +0100

    Renderbackend: prepare separation of text rendering.

---
+++ src/Engine/RenderBackend/TextRenderer.cpp
@@ -0,0 +1,15 @@
+#include "TextRenderer.h"
+
+namespace OSRE {
+namespace RenderBackend {
+
+TextRenderer::TextRenderer() {
+
+}
+
+TextRenderer::~TextRenderer() {
+
+}
+
+} // Namespace RenderBackend
+} // Namespace OSRE
