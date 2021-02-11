af4d279bfe122482d41d6011d0b7388234a805e0
Kim Kulling <kimkulling@users.noreply.github.com>
2021-02-11 12:32:45 +0100

    Update HelloWorld.cpp

--- samples/00_HelloWorld/HelloWorld.cpp
+++ samples/00_HelloWorld/HelloWorld.cpp
@@ -20,8 +20,6 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 -----------------------------------------------------------------------------------------------*/
-#include <osre/App/AppBase.h>
-#include <osre/App/AssetRegistry.h>
 #include <osre/App/Component.h>
 #include <osre/App/Entity.h>
 #include <osre/App/World.h>
