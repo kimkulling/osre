606982494ef8dc23c67b26bb2acd29d329da263b
Kim Kulling <kim.kulling@googlemail.com>
2021-10-31 11:03:16 +0100

    Fix references of Ray.

--- test/UnitTests/src/Common/RayTest.cpp
+++ test/UnitTests/src/Common/RayTest.cpp
@@ -21,7 +21,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 -----------------------------------------------------------------------------------------------*/
 #include <gtest/gtest.h>
-#include <osre/Common/TRay.h>
+#include <osre/Common/Ray.h>
 
 namespace OSRE {
 namespace UnitTest {
