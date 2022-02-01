505963709f7e52375da60dc2ed45c27b24c91d98
Kim Kulling <kimkulling@users.noreply.github.com>
2022-02-01 09:48:25 +0100

    Introduce public header for App-Layer

---
+++ include/osre/App/App.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// The public API from the App-layer
+#include <osre/App/AppBase.h>
+#include <osre/App/Component.h>
+#include <osre/App/Entity.h>
+#include <osre/App/World.h>
+#include <osre/App/AppCommon.h>
+#include <osre/App/AssimpWrapper.h>
+#include <osre/App/IModuleView.h>
+#include <osre/App/MOduleBase.h>
+#include <osre/App/Project.h>
+#include <osre/App/ServiceProvider.h>
+#include <osre/App/Stage.h>
+#include <osre/App/TAbstractCtrlBase.h>
