b610d3e0b23c329539c04965d2f36c25e07b5114
Kim Kulling <kim.kulling@googlemail.com>
2022-04-11 11:12:32 +0200

    add missing headers

---
+++ include/osre/Scene/Scene.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <osre/Scene/MeshBuilder.h>
+#include <osre/Scene/Camera.h>
+#include <osre/Scene/Node.h>
+#include <osre/Scene/MaterialBuilder.h>
+#include <osre/Scene/DbgRenderer.h>
+#include <osre/Scene/Node.h>
