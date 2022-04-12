0148064613a1393a5db6b204fda824199dac1e3b
Kim Kulling <kim.kulling@googlemail.com>
2022-04-12 10:39:10 +0200

    Fix lower-uppercase error

--- include/osre/App/App.h
+++ include/osre/App/App.h
@@ -30,7 +30,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <osre/App/AppCommon.h>
 #include <osre/App/AssimpWrapper.h>
 #include <osre/App/IModuleView.h>
-#include <osre/App/MOduleBase.h>
+#include <osre/App/ModuleBase.h>
 #include <osre/App/Project.h>
 #include <osre/App/ServiceProvider.h>
 #include <osre/App/AssetRegistry.h>
