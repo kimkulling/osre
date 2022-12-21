6bbd213fe57ec5d7bc2de654466170718c102302
Kim Kulling <kimkulling@users.noreply.github.com>
2022-12-21 16:24:03 +0100

    Fix: Fix broken search and replace

--- src/Engine/RenderBackend/OGLRenderer/OGLRenderBackend.cpp
+++ src/Engine/RenderBackend/OGLRenderer/OGLRenderBackend.cpp
@@ -1,7 +1,7 @@
 /*-----------------------------------------------------------------------------------------------
 The MIT License (MIT)
 
-Copyright (c) 2015-2021 OSRE ( Open Source Render Engine ) by Kim Kulling
+Copyright (c) 2015-2022 OSRE ( Open Source Render Engine ) by Kim Kulling
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -35,7 +35,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <osre/RenderBackend/RenderStates.h>
 #include <osre/RenderBackend/Shader.h>
 
-#include <cppcore/cppcoreCommon.h>
+#include <cppcore/CPPCoreCommon.h>
 #include <cppcore/Memory/MemUtils.h>
 
 #include "stb_image.h"
