3b4e58e6959f743a2925b0190782defb9196b19b
Kim Kulling <kim.kulling@googlemail.com>
2023-01-03 21:24:07 +0100

    Update: Add 2023 to copyright

--- src/Engine/Platform/win32/Win32OSService.cpp
+++ src/Engine/Platform/win32/Win32OSService.cpp
@@ -1,7 +1,7 @@
 /*-----------------------------------------------------------------------------------------------
 The MIT License (MIT)
 
-Copyright (c) 2015-2022 OSRE ( Open Source Render Engine ) by Kim Kulling
+Copyright (c) 2015-2023 OSRE ( Open Source Render Engine ) by Kim Kulling
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
