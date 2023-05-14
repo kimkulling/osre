a2778d95c42e7b2451320fd94496e4893e62f645
Kim Kulling <kim.kulling@googlemail.com>
2023-05-14 19:16:27 +0200

    Fix: remove header

--- src/Editor_imgui/src/main.cpp
+++ src/Editor_imgui/src/main.cpp
@@ -1,14 +1,9 @@
-#include <iostream>
-#include "imgui.h"
 #include "backends/imgui_impl_sdl2.h"
 #include "backends/imgui_impl_opengl3.h"
 #include <stdio.h>
 #include <SDL.h>
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-#include <SDL_opengles2.h>
-#else
 #include <SDL_opengl.h>
-#endif
+#include <iostream>
 
 #define main main
 
@@ -95,9 +90,6 @@ public:
 
         AppBase::onUpdate();
     }
-
-
-
 };
 
 int main(int argc, char *argv[]) {
