78ae75be5fbe2180081d709712849f7b9ab9808d
Kim Kulling <kim.kulling@googlemail.com>
2024-02-09 20:13:29 +0100

    Add missing files

---
+++ src/Engine/RenderBackend/2D/RenderPass2D.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <osre/RenderBackend/RenderPass.h>
+
+namespace OSRE {
+namespace RenderBackend {
+
+    class RenderPass2D {
+    public:
+        RenderPass2D();
+        ~RenderPass2D();
+        RenderPass *build();
+    };
+
+    inline RenderPass2D::RenderPass2D() {}
+    inline RenderPass2D::~RenderPass2D() {}
+    inline RenderPass *RenderPass2D::build() {
+        Shader 
+        RenderPass *pass = new RenderPass();
+    }
+
+    } // namespace RenderBackend
+} // namespace OSRE
