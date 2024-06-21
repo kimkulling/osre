3f5516b41cd1b30e794ec05964022cf089d4f882
Kim Kulling <kimkulling@users.noreply.github.com>
2024-06-21 19:45:14 +0200

    Update SDL2DisplayInterface.cpp

--- src/Engine/Platform/sdl2/SDL2DisplayInterface.cpp
+++ src/Engine/Platform/sdl2/SDL2DisplayInterface.cpp
@@ -21,7 +21,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 -----------------------------------------------------------------------------------------------*/
 #include "SDL2DisplayInterface.h"
-#include <osre/Common/Logger.h>
+#include "Common/Logger.h"
 #include "SDL.h"
 
 namespace OSRE {
