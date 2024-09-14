32a0761a790bed73672e643f7b5d96a3f34b80a9
Kim Kulling <kimkulling@users.noreply.github.com>
2024-09-14 09:40:17 +0200

    Update TransformMatrixBlock.cpp
    Fix review finding.

--- src/Engine/RenderBackend/TransformMatrixBlock.cpp
+++ src/Engine/RenderBackend/TransformMatrixBlock.cpp
@@ -25,12 +25,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 namespace OSRE {
 namespace RenderBackend {
 
-TransformMatrixBlock::TransformMatrixBlock() :
-        mProjection(),
-        mModel(),
-        mView(),
-        mNormal(),
-        mMvp() {
+TransformMatrixBlock::TransformMatrixBlock() {
     init();
 }
 
