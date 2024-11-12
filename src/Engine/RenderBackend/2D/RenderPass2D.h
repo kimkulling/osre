4f5557db11ca7396768970670f1029e0800e4858
Kim Kulling <kim.kulling@googlemail.com>
2024-11-12 23:21:08 +0100

    Remove dead code

--- src/Engine/RenderBackend/2D/RenderPass2D.h
+++ src/Engine/RenderBackend/2D/RenderPass2D.h
@@ -37,8 +37,6 @@ public:
     RenderPass2D();
     ~RenderPass2D();
     static RenderPass *build(guid id);
-    //static const String &getVertexShader();
-    //static const String &getFragmentShader();
 };
 
 inline RenderPass2D::RenderPass2D() {}
