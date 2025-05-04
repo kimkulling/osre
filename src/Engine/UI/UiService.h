54213b021b9b0320f44ac27ee88d127e91a1e9f9
Kim Kulling <kim.kulling@draeger.com>
2025-05-04 13:45:44 +0200

    Add missing files

---
+++ src/Engine/UI/UiService.h
@@ -0,0 +1,56 @@
+/*-----------------------------------------------------------------------------------------------
+The MIT License (MIT)
+
+Copyright (c) 2015-2025 OSRE ( Open Source Render Engine ) by Kim Kulling
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-----------------------------------------------------------------------------------------------*/
+#pragma once
+
+#include "Common/AbstractService.h"
+
+namespace OSRE {
+namespace RenderBackend {
+    class CanvasRenderer;
+}
+}
+
+namespace OSRE::Ui {
+        
+    class OSRE_EXPORT UiService : public Common::AbstractService {
+    public:
+        DECLARE_SINGLETON(UiService)
+
+    public:
+        UiService();
+        ~UiService();
+        void setCanvasRenderer(RenderBackend::CanvasRenderer *canvasRenderer);
+        /// @brief  Will create a new instance.
+        /// @return The new created instance.
+        static UiService *create();
+
+
+    protected:
+        bool onOpen() override;
+        bool onClose() override;
+        bool onUpdate() override;
+
+    private:    
+        RenderBackend::CanvasRenderer *mCanvasRenderer;
+    };
+}
